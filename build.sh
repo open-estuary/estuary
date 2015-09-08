8a38dbba48dfb737551f4e0cf08c39b13ad0828e
justin.zhao <zhaojunhua@hisilicon.com>
2015-09-09 00:43:23 +0800

    Install wget for first time
    Signed-off-by: justin.zhao <zhaojunhua@hisilicon.com>

--- build.sh
+++ build.sh
@@ -220,7 +220,7 @@ if [ x"$?" = x"1" ]; then
 fi
 
 if [ ! -f ".initilized" ]; then
-    sudo apt-get install -y automake1.11 make bc libncurses5-dev libtool libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 bison flex
+    sudo apt-get install -y wget automake1.11 make bc libncurses5-dev libtool libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 bison flex
     if [ x"$?" = x"0" ]; then
         touch ".initilized"
     fi
