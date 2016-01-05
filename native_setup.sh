96d23ebbbb2348c0b6e4838dbf830bd7b8085578
wangyanliang <wylgm2012@gmail.com>
2016-01-05 17:02:32 +0800

    suppor native build and deployment

--- native_setup.sh
+++ native_setup.sh
@@ -8,53 +8,51 @@ cwd=`dirname $0`
 
 echo "--------------------------------------------------------------------------------"
 
-#copy/paste programs
-cp_progress ()
-{
-	CURRENTSIZE=0
-	while [ $CURRENTSIZE -lt $TOTALSIZE ]
-	do
-		TOTALSIZE=$1;
-		TOHERE=$2;
-		CURRENTSIZE=`sudo du -c $TOHERE | grep total | awk {'print $1'}`
-		echo -e -n "$CURRENTSIZE /  $TOTALSIZE copied \r"
-		sleep 1
-	done
-}
+to_install_packages="jq parted dosfstools"
+install_packages to_install_packages
 
-cat << EOM
-Begin to parse estuary.cfg ...
-EOM
-while read line
+default_config=$cwd/estuarycfg.json
+echo "--------------------------------------------------------------------------------"
+echo "Please specify estuary config file (press return to use:$default_config)"
+read -p "[ $default_config ] " CFGFILE
+
+if [ ! -n "$CFGFILE" ]; then
+    CFGFILE=$default_config
+fi
+echo "--------------------------------------------------------------------------------"
+parse_config $CFGFILE
+
+DISTROS=()
+idx=0
+idx_en=0
+install=`jq -r ".distros[$idx].install" $CFGFILE`
+while [ x"$install" != x"null" ];
 do
-    name=`echo $line | awk -F '=' '{print $1}'`
-    value=`echo $line | awk -F '=' '{print $2}'`
+    if [ x"yes" = x"$install" ]; then
+        idx_en=${#DISTROS[@]}
+        DISTROS[${#DISTROS[@]}]=`jq -r ".distros[$idx].name" $CFGFILE`
+    fi
+    name=`jq -r ".distros[$idx].name" $CFGFILE`
+    value=`jq -r ".distros[$idx].install" $CFGFILE`
     case $name in
-        "arch")
-        TARGET_ARCH=$value
-        ;;
-        "platform")
-        build_PLATFORM=$value
-        ;;
-        "distro")
-        build_DISTRO=$value
-        ;;
-        "ubuntu")
+        "Ubuntu")
         ubuntu_en=$value
         ;;
-        "opensuse")
+        "OpenSuse")
         opensuse_en=$value
         ;;
-        "fedora")
+        "Fedora")
         fedora_en=$value
         ;;
-        "debian")
+        "Debian")
         debian_en=$value
         ;;
         *)
         ;;
     esac
-done < estuary.cfg
+    let idx=$idx+1
+    install=`jq -r ".distros[$idx].install" $CFGFILE`
+done
 
 pwd=`pwd`
 pushd ..
@@ -70,48 +68,7 @@ then
     fi
 fi
 popd
-
-mkdir -p /sys_setup/boot/EFI/GRUB2 2> /dev/null
-mkdir -p /sys_setup/distro 2> /dev/null
-mkdir -p /sys_setup/bin 2> /dev/null
-cp -a $cwd/../build/$build_PLATFORM/binary/grubaa64* /sys_setup/boot/EFI/GRUB2
-cp -a $cwd/../build/$build_PLATFORM/binary/Image_$build_PLATFORM /sys_setup/boot/Image
-cp -a $cwd/../build/$build_PLATFORM/binary/hip05-d02.dtb /sys_setup/boot
-if [ "$ubuntu_en" == "y" ]; then
-    mkdir -p /sys_setup/distro/$build_PLATFORM/ubuntu$TARGET_ARCH 2> /dev/null
-    TOTALSIZE=`sudo du -c ../distro/Ubuntu_"$TARGET_ARCH".tar.gz | grep total | awk {'print $1'}`
-    cp -af $cwd/../distro/Ubuntu_"$TARGET_ARCH".tar.gz /sys_setup/distro/$build_PLATFORM/ubuntu$TARGET_ARCH &
-    cp_progress $TOTALSIZE /sys_setup/distro/$build_PLATFORM/ubuntu$TARGET_ARCH
-fi
-if [ "$fedora_en" == "y" ]; then
-    pushd ..
-    if [ -f $PWD/estuary/build.sh ]; then
-        $PWD/estuary/build.sh -p $build_PLATFORM -d Fedora
-    fi
-    popd
-    mkdir -p /sys_setup/distro/$build_PLATFORM/fedora$TARGET_ARCH 2> /dev/null
-    cp -a $cwd/../distro/Fedora_"$TARGET_ARCH".tar.gz /sys_setup/distro/$build_PLATFORM/fedora$TARGET_ARCH
-fi
-if [ "$debian_en" == "y" ]; then
-    pushd ..
-    if [ -f $PWD/estuary/build.sh ]; then
-        $PWD/estuary/build.sh -p $build_PLATFORM -d Debian
-    fi
-    popd
-    mkdir -p /sys_setup/distro/$build_PLATFORM/debian$TARGET_ARCH 2> /dev/null
-    cp -a $cwd/../distro/Debian_"$TARGET_ARCH".tar.gz /sys_setup/distro/$build_PLATFORM/debian$TARGET_ARCH
-fi
-if [ "$opensuse_en" == "y" ]; then
-    pushd ..
-    if [ -f $PWD/estuary/build.sh ]; then
-        $PWD/estuary/build.sh -p $build_PLATFORM -d OpenSuse
-    fi
-    popd
-    mkdir -p /sys_setup/distro/$build_PLATFORM/opensuse$TARGET_ARCH 2> /dev/null
-    cp -a $cwd/../distro/OpenSuse_"$TARGET_ARCH".tar.gz /sys_setup/distro/$build_PLATFORM/opensuse$TARGET_ARCH
-fi
-
-cp -a post_install.sh /sys_setup/bin
+    cp_distros
 
 sudo ./sys_setup.sh
 
