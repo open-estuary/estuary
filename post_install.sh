9c329d5488ab671b85c7a6dfad456dbdb11499f8
Shiju Jose <shiju.jose@huawei.com>
2016-04-25 12:06:02 +0100

    fix for the tool's modules which are not automatically installed on target

--- post_install.sh
+++ post_install.sh
@@ -59,3 +59,50 @@ do
 done
 #enable function_graph as default tracer
 echo function_graph > /sys/kernel/debug/tracing/current_tracer
+
+# Workaround for the tools modules which are not getting installed on rootfs automatically.
+depmod -a
+modprobe lttng-probe-kvm
+modprobe lttng-probe-sock
+modprobe lttng-probe-asoc
+modprobe lttng-ring-buffer-metadata-client
+modprobe lttng-probe-printk
+modprobe lttng-probe-napi
+modprobe lttng-probe-v4l2
+modprobe lttng-statedump
+modprobe lttng-probe-btrfs
+modprobe lttng-ring-buffer-client-mmap-discard
+modprobe lttng-probe-kmem
+modprobe lttng-probe-compaction
+modprobe lttng-ring-buffer-client-overwrite
+modprobe lttng-ring-buffer-client-mmap-overwrite
+modprobe lttng-probe-sunrpc
+modprobe lttng-ftrace
+modprobe lttng-probe-signal
+modprobe lttng-probe-module
+modprobe lttng-ring-buffer-client-discard
+modprobe lttng-probe-timer
+modprobe lttng-types
+modprobe lttng-probe-net
+modprobe lttng-probe-writeback
+modprobe lttng-probe-gpio
+modprobe lttng-probe-udp
+modprobe lttng-ring-buffer-metadata-mmap-client
+modprobe lttng-lib-ring-buffer
+modprobe lttng-probe-jbd2
+modprobe lttng-probe-statedump
+modprobe lttng-probe-ext4
+modprobe lttng-probe-rcu
+modprobe lttng-tracer
+modprobe lttng-probe-power
+modprobe lttng-probe-sched
+modprobe lttng-probe-block
+modprobe lttng-probe-vmscan
+modprobe lttng-probe-scsi
+modprobe lttng-probe-regmap
+modprobe lttng-probe-skb
+modprobe lttng-probe-regulator
+modprobe lttng-probe-random
+modprobe lttng-probe-workqueue
+modprobe lttng-probe-irq
+
