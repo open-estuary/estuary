76e4ef2eb2b7081d2a31656d57f67d67d0568224
cailianchun <armstar@sina.com>
2016-07-05 16:20:02 +0800

    Add arm32 toolchain for uefi.
    1. Add arm32 toolchain for uefi needed by hikey.
    
    Signed-off-by: cailianchun <armstar@sina.com>

--- include/toolchain-func.sh
+++ include/toolchain-func.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
 ###################################################################################
-# get_toolchain <toolchainsum_source_file>
+# get_toolchain <arch> <toolchainsum_source_file>
 ###################################################################################
 get_toolchain()
 {
 	(
-	toolchainsum_source_file=$1
-	cat $toolchainsum_source_file 2>/dev/null | awk '{print $2}' | sed 's/.*\///'
+	arch=$1
+	toolchainsum_source_file=$2
+	cat $toolchainsum_source_file 2>/dev/null | grep $arch | awk '{print $2}' | sed 's/.*\///'
 	)
 }
 
 ###################################################################################
 # download_toolchains <target_dir> <toolchainsum_source_file> <toolchain_source>
 ###################################################################################
-download_toolchain()
+download_toolchains()
 {
 	(
 	target_dir=$(cd $1; pwd)
 	toolchainsum_source_file=$2
 	toolchain_source=$3
-	toolchain=`cat $toolchainsum_source_file 2>/dev/null | awk '{print $2}'`
+	
+	checksum_file=`basename $toolchainsum_source_file`
+	checksum_dir=`dirname $toolchainsum_source_file`
+	checksum_dir=`cd $checksum_dir; pwd`
 
-	if ! check_sum $target_dir $toolchainsum_source_file >/dev/null 2>&1; then
+	if ! check_sum $target_dir $checksum_dir/$checksum_file >/dev/null 2>&1; then
 		pushd $target_dir >/dev/null
-		wget $toolchain_source/$toolchain || return 1
+		toolchain_files=(`md5sum --quiet --check $checksum_dir/$checksum_file 2>/dev/null | grep "FAILED" | cut -d : -f 1`)
+		for toolchain_file in ${toolchain_files[*]}; do
+			rm -f $toolchain_file 2>/dev/null
+			wget -c $toolchain_source/$toolchain_file || return 1
+		done
 		popd >/dev/null
 	fi
 
@@ -42,7 +50,7 @@ copy_toolchain()
 	src_dir=$2
 	target_dir=$3
 	if ! (diff $src_dir/.toolchain.sum $target_dir/.toolchain.sum >/dev/null 2>&1) \
-		|| [ !- f $target_dir/$toolchain ]; then
+		|| [ ! -f $target_dir/$toolchain ]; then
 		rm -f $target_dir/$toolchain 2>/dev/null
 		cp $src_dir/$toolchain $target_dir/$toolchain || return 1
 		rm -f $target_dir/.toolchain.sum 2>/dev/null
@@ -53,4 +61,26 @@ copy_toolchain()
 	)
 }
 
+###################################################################################
+# uncompress_toolchains <toolchainsum_source_file> <src_dir>
+###################################################################################
+uncompress_toolchains()
+{
+	(
+	toolchainsum_source_file=$1
+	src_dir=$2
+
+	toolchain_files=(`cat $toolchainsum_source_file 2>/dev/null | awk '{print $2}' | sed 's/.*\///'`)
+	for toolchain_file in ${toolchain_files[*]}; do
+		toolchain_dir=`get_compress_file_prefix $toolchain_file`
+		if [ ! -d $src_dir/$toolchain_dir ]; then
+			if ! uncompress_file $src_dir/$toolchain_file $src_dir; then
+				rm -rf $src_dir/$toolchain_dir 2>/dev/null ; return 1
+			fi
+		fi
+	done
+
+	return 0
+	)
+}
 
