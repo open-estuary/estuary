1d947609c97034fe131b53b2d07f5d90e3811df1
cailianchun <armstar@sina.com>
2016-09-07 10:20:16 +0800

    Add doc softlink from comm doc for platfrom.
    Signed-off-by: cailianchun <armstar@sina.com>

--- include/doc-func.sh
+++ include/doc-func.sh
@@ -28,13 +28,17 @@ copy_all_docs()
 	src_dir=$2
 	target_dir=$3
 
+	mkdir -p $target_dir/arm64
+	copy_plat_doc All $doc_src_dir $target_dir/arm64 || return 1
+
 	for plat in ${platfroms[*]}; do
 		mkdir -p $target_dir/$plat
 		copy_plat_doc $plat $doc_src_dir $target_dir/$plat || return 1
+		pushd $target_dir/$plat >/dev/null
+		find . -maxdepth 1 -type l -print | xargs rm -f
+		find ../arm64/ -maxdepth 1 -type f | xargs -i ln -s {}
+		popd >/dev/null
 	done
-
-	mkdir -p $target_dir/arm64
-	copy_plat_doc All $doc_src_dir $target_dir/arm64 || return 1
 
 	return 0
 	)
