bad4d55e7342a3e8e4a0e43e075489f4e5ed50ef
wuyanjun <wuyanjun1027@163.com>
2017-03-22 11:25:46 +0800

    fix the bug of parted set boot on partition 1

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -170,7 +170,7 @@ else
 
         if [ ${#INSTALL_DISTRO[@]} -eq 0 ]; then
             echo "You have not selected any distro again! Will reboot now."
-            reboot
+            reboot -f
         fi
     fi
 fi
@@ -266,6 +266,7 @@ efi_end_address=$(( start_address + BOOT_PARTITION_SIZE))
 BOOT_DEV=
 (parted -s $TARGET_DISK "mkpart UEFI $efi_start_address $efi_end_address") >/dev/null 2>&1
 (parted -s $TARGET_DISK set 1 boot on) >/dev/null 2>&1
+/sbin/mdev -s
 first_part=`lsblk ${TARGET_DISK} -ln -o NAME,TYPE | grep -m 1 part | awk '{print $1}'`
 PART_PREFIX=`echo "/dev/$first_part" | sed 's/[0-9]*$//g' | sed "s,^${TARGET_DISK},,g"`
 BOOT_DEV="${TARGET_DISK}${PART_PREFIX}1"
