284e2daa0c16bb30e39050906679c92f864d962c
linwenkai <941116795@qq.com>
2017-12-25 22:17:33 +0800

    remove priority of CentOS repo Base repo will be conflict with linaro repo anyway.
    Signed-off-by: linwenkai <941116795@qq.com>

--- submodules/centos-build-installer.sh
+++ submodules/centos-build-installer.sh
@@ -46,7 +46,6 @@ network  --bootproto=dhcp --device=eth0 --ipv6=auto --no-activate
 text
 
 %packages
-yum-plugin-priorities
 bash-completion
 epel-release
 wget
@@ -57,10 +56,6 @@ wget -O /etc/yum.repos.d/estuary.repo https://raw.githubusercontent.com/open-est
 chmod +r /etc/yum.repos.d/estuary.repo
 rpm --import http://repo.estuarydev.org/releases/ESTUARY-GPG-KEY
 wget http://repo.linaro.org/rpm/linaro-overlay/centos-7/linaro-overlay.repo -O /etc/yum.repos.d/linaro-overlay.repo
-find /etc/yum.repos.d/ -name  "CentOS-Base*"|xargs sed -i '/gpgcheck=*/a\priority=3'
-find /etc/yum.repos.d/ -name  "estuary*"|xargs sed -i '/gpgcheck=*/a\priority=1'
-find /etc/yum.repos.d/ -name  "epel*"|xargs sed -i '/gpgcheck=*/a\priority=5'
-find /etc/yum.repos.d/ -name  "linaro*"|xargs sed -i '/gpgcheck=*/a\priority=1'
 yum clean dbcache
 %end
 EOF
