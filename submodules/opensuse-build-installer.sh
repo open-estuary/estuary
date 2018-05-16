f0b926349ac3e2b1cee89c051cf97b7968126ac6
linwenkai <941116795@qq.com>
2018-05-16 03:25:16 -0800

    replace kernel of opensuse installer Modify repo url for opensuse install
    Signed-off-by: linwenkai <941116795@qq.com>

--- submodules/opensuse-build-installer.sh
+++ submodules/opensuse-build-installer.sh
@@ -11,10 +11,12 @@ out=${build_dir}/out/release/${version}/OpenSuse
 release_dir=${build_dir}/out/release/${version}/OpenSuse/netboot
 distro_dir=${build_dir}/tmp/opensuse
 workspace=${distro_dir}/installer
-netiso_url=${OPENSUSE_ISO_MIRROR:-"http://ftp.jaist.ac.jp/pub/Linux/openSUSE/ports/aarch64/distribution/leap/42.3/iso/"}
+netiso_url=${OPENSUSE_ISO_MIRROR:-"http://download.opensuse.org/ports/aarch64/distribution/leap/42.3/iso/"}
 opensuse_url=${OPENSUSE_MIRROR:-"http://htsat.vicp.cc:804/opensuse"}
 ISO=openSUSE-Leap-42.3-NET-aarch64-Build0200-Media.iso
 
+kernel_abi="4.16.3-0.gd41301c"
+rpm -ivh --root=/  ${opensuse_url}/kernel-default-${kernel_abi}.aarch64.rpm
 rm -rf ${workspace}
 mkdir -p ${workspace} && cd ${workspace}
 mkdir -p opensuse-installer
@@ -35,6 +37,20 @@ cd ${workspace}/opensuse-installer; rm -rf netinstall
 xorriso -osirrox on -indev ${iso_dir}/${ISO} -extract / netinstall
 cd netinstall
 sed -i 's/silent.*/& autoyast=http:\/\/htsat.vicp.cc:804\/opensuse\/autoinst.xml/' EFI/BOOT/grub.cfg
+
+cp /boot/Image-${kernel_abi}-default boot/aarch64/linux
+mkdir initrd; cd initrd
+sh -c 'xzcat ../boot/aarch64/initrd | cpio -d -i -m -u'
+sed -i "s#http://download.opensuse.org/#http://download.opensuse.org/ports/aarch64/#g" linuxrc.config
+rm -rf modules
+mkdir -p lib/modules/${kernel_abi}-default/initrd
+ln -sf lib/modules/${kernel_abi}-default/initrd modules
+find /lib/modules/${kernel_abi}-default -name "loop.ko"|xargs -i cp -v {} modules/
+find /lib/modules/${kernel_abi}-default -name "squashfs.ko"|xargs -i cp -v {} modules/
+cp -rvf /lib/modules/${kernel_abi}-default lib/modules/
+sh -c 'find . | cpio -o -H newc | xz --check=crc32 --lzma2=dict=512KiB > ../boot/aarch64/initrd'
+cd ..; rm -rf initrd
+
 mkisofs  -R -o boot.iso -b boot/aarch64/efi -c boot.catalog -no-emul-boot .
 
 # Final preparation for publishing
--- submodules/opensuse-build-iso.sh
+++ submodules/opensuse-build-iso.sh
@@ -10,9 +10,12 @@ WGET_OPTS="-T 120 -c"
 out=${build_dir}/out/release/${version}/OpenSuse
 distro_dir=${build_dir}/tmp/opensuse
 kernel_rpm_dir=${build_dir}/out/kernel-pkg/${version}/opensuse
-dvdiso_url=${OPENSUSE_ISO_MIRROR:-"http://ftp.jaist.ac.jp/pub/Linux/openSUSE/ports/aarch64/distribution/leap/42.3/iso/"}
+dvdiso_url=${OPENSUSE_ISO_MIRROR:-"http://download.opensuse.org/ports/aarch64/distribution/leap/42.3/iso/"}
 opensuse_url=${OPENSUSE_MIRROR:-"http://htsat.vicp.cc:804/opensuse"}
 ISO=openSUSE-Leap-42.3-DVD-aarch64-Build0200-Media.iso
+
+kernel_abi="4.16.3-0.gd41301c"
+rpm -ivh --root=/  ${opensuse_url}/kernel-default-${kernel_abi}.aarch64.rpm
 
 # download ISO
 iso_dir=/root/iso
@@ -31,6 +34,20 @@ cd ${kernel_rpm_dir} ; wget ${WGET_OPTS} -r -nd -np -L -A *.aarch64.rpm ${opensu
 cd ${distro_dir}; rm -rf dvdiso
 xorriso -osirrox on -indev ${iso_dir}/${ISO} -extract / dvdiso
 cd dvdiso
+
+cp /boot/Image-${kernel_abi}-default boot/aarch64/linux
+mkdir initrd; cd initrd
+sh -c 'xzcat ../boot/aarch64/initrd | cpio -d -i -m -u'
+sed -i "s#http://download.opensuse.org/#http://download.opensuse.org/ports/aarch64/#g" linuxrc.config
+rm -rf modules
+mkdir -p lib/modules/${kernel_abi}-default/initrd
+ln -sf lib/modules/${kernel_abi}-default/initrd modules
+find /lib/modules/${kernel_abi}-default -name "loop.ko"|xargs -i cp -v {} modules/
+find /lib/modules/${kernel_abi}-default -name "squashfs.ko"|xargs -i cp -v {} modules/
+cp -rvf /lib/modules/${kernel_abi}-default lib/modules/
+sh -c 'find . | cpio -o -H newc | xz --check=crc32 --lzma2=dict=512KiB > ../boot/aarch64/initrd'
+cd ..; rm -rf initrd
+
 rm -rf suse/aarch64/kernel-*
 cp ${kernel_rpm_dir}/kernel-default* suse/aarch64/
 rm -rf suse/setup/descr/packages.*
