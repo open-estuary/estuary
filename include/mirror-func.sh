1b16e00793feb24495cc90d96cf28aaa065e96c6
linwenkai <941116795@qq.com>
2018-07-06 04:02:49 +0200

    Fix add estuary key problem
    Signed-off-by: linwenkai <941116795@qq.com>

--- include/mirror-func.sh
+++ include/mirror-func.sh
@@ -1,7 +1,8 @@
 set_debian_mirror()
 {
 	rm -rf /etc/apt/sources.list.d/*.list
-	wget -O - ${ESTUARY_REPO}/ESTUARY-GPG-KEY | apt-key add -
+	wget -O - ${ESTUARY_REPO}/ESTUARY-GPG-KEY | \
+	    apt-key --keyring /usr/share/keyrings/debian-archive-keyring.gpg add -
 	wget -O /etc/apt/sources.list.d/estuary.list https://raw.githubusercontent.com/open-estuary/distro-repo/master/estuaryftp_debian.list
 	if [ -n "${DEBIAN_MIRROR}" ]; then
 		local default_mirror="http://deb.debian.org/debian"
