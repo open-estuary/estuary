39e970139fe600daa36e4241d90e1a9658fe6291
linwenkai <941116795@qq.com>
2018-08-08 03:29:26 +0200

    Remove unused codes of fedora script
    Signed-off-by: linwenkai <941116795@qq.com>

--- submodules/fedora-calculate-md5sum.sh
+++ submodules/fedora-calculate-md5sum.sh
@@ -5,14 +5,8 @@ set -ex
 top_dir=$(cd `dirname $0`; cd ..; pwd)
 build_dir=$(cd /root/$2 && pwd)
 version=$1 # branch or tag
-
 workspace=${build_dir}/out/release/${version}/Fedora
 
-# Update fedora repo
-. ${top_dir}/include/mirror-func.sh
-set_fedora_mirror
-
 . ${top_dir}/include/checksum-func.sh
-
 rm -f $workspace/*.MD5SUM
 cal_md5sum ${workspace}
